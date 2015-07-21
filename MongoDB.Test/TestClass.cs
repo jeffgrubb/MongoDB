789e9ba72d6286fd2cccc0878f133f26d26ad350
Scapes23 <jeff@jeffgrubb.org>
2015-07-20 21:01:20 -0400

    Added nunit testing framework

---
+++ MongoDB.Test/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using MongoDB;
+
+
+namespace MongoDB.Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void TestDatabase()
+        {
+            MongoDatabase db = new MongoDatabase();
+            db.GetCollection("foo");
+        }
+    }
+}
